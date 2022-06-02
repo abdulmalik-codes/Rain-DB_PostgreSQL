ff3b41e795739553e32ae2f6024bb75a86a382af
Abdul-Malik Mohamed <abdul-malik.mohamed@rain.co.za>
2022-06-02 11:20:21 +0200

    code dump

--- Backend/routes/forgotPassword.js
+++ Backend/routes/forgotPassword.js
@@ -82,4 +82,79 @@ router
     }
   );
 
+// ******************************************* //
+router
+  .route("/admin")
+  // request new password
+  .put(
+    [check("email", "Please enter a valid email!").isEmail()],
+    (request, response, next) => {
+      const { email, password, password2 } = request.body;
+
+      pool.query(
+        `SELECT email FROM admin WHERE email=($1)`,
+        [email],
+        async (err, res) => {
+          if (err) return next(err);
+
+          if (!email || email === " ") {
+            response.json(`Please provide an email address!`);
+          } else {
+            const errors = validationResult(request);
+
+            if (!errors.isEmpty()) {
+              return response.status(400).json({
+                errors: errors.array(),
+              });
+            }
+
+            if (res.rows.length === 0) {
+              response.json(`${email} does not exist!`);
+            } else {
+              if (
+                !password ||
+                !password2 ||
+                password === " " ||
+                password2 === " "
+              ) {
+                response.json(
+                  `Input values are missing! Please provide passwords!`
+                );
+              } else {
+                // set new password here
+
+                if (password !== password2) {
+                  response.json(`Passwords do not match!`);
+                } else {
+                  const hashedPassword = await bcrypt.hash(password, 10);
+
+                  pool.query(
+                    `UPDATE admin SET password=($1) WHERE email=($2)`,
+                    [hashedPassword, email],
+                    (err, res) => {
+                      if (err) return next(err);
+                    }
+                  );
+
+                  let forgotPasswordAdminEmail = {
+                    from: "62545a@gmail.com",
+                    to: `${email}`,
+                    subject: `RainSA - Password reset successfully!`,
+                    text: `Password reset to: "${password}", login with your email and new password here: ${rainUrl} `,
+                  };
+
+                  const info = await transporter.sendMail(
+                    forgotPasswordAdminEmail
+                  );
+
+                  response.json(`Please check your email!`);
+                }
+              }
+            }
+          }
+        }
+      );
+    }
+  );
+
 module.exports = router;
